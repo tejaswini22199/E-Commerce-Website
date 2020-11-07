424f000661ea760044df905275d60dfa6d7c4044
AJAY BABU SAPPA <jysappa@gmail.com>
2020-11-07 14:52:29 +0530

    changes

--- login/admin.py
+++ login/admin.py
@@ -1,5 +1,5 @@
-from mysite.login.models import sellproduct
+from .models import sellproduct
 from django.contrib import admin
 
 # Register your models here.
-admin.site.register('sellproduct')
+admin.site.register(sellproduct)
